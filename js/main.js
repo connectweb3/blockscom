438e7767db53e3c712cc1b5c8d4b052d7da4b7d3
Junkora <evelinneliaelefante12@gmail.com>
2025-12-03 23:49:57 +0800

    test

--- js/main.js
+++ js/main.js
@@ -41,41 +41,6 @@ document.querySelectorAll('a, button, input').forEach(el => {
 // Animations
 document.addEventListener('DOMContentLoaded', () => {
 
-    // Burger Menu Logic
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav-links');
-    const navLinks = document.querySelectorAll('.nav-links li');
-
-    if (burger) {
-        burger.addEventListener('click', () => {
-            // Toggle Nav
-            nav.classList.toggle('nav-active');
-
-            // Animate Links
-            navLinks.forEach((link, index) => {
-                if (link.style.animation) {
-                    link.style.animation = '';
-                } else {
-                    link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
-                }
-            });
-
-            // Burger Animation
-            burger.classList.toggle('toggle');
-        });
-
-        // Close menu when link is clicked
-        navLinks.forEach(link => {
-            link.addEventListener('click', () => {
-                nav.classList.remove('nav-active');
-                burger.classList.remove('toggle');
-                navLinks.forEach(link => {
-                    link.style.animation = '';
-                });
-            });
-        });
-    }
-
     // Hero Text Reveal
     gsap.from('.split-text', {
         y: 100,
